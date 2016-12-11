9644441b5ff8ce977b0d6a7698551c1be587bd0f
aurieh <auriteh@gmail.com>
2016-12-11 19:18:09 +0200

    Patch uwu URLs and bump

--- routes/shorten/polr.js
+++ routes/shorten/polr.js
@@ -8,6 +8,7 @@ const util = require('../../lib/util.js');
 // URL regex (taken from https://gist.github.com/dperini/729294)
 // Credit to @dperini
 const URLRegex = /^(?:(?:https?|ftp):\/\/)(?:\S+(?::\S*)?@)?(?:(?!(?:10|127)(?:\.\d{1,3}){3})(?!(?:169\.254|192\.168)(?:\.\d{1,3}){2})(?!172\.(?:1[6-9]|2\d|3[0-1])(?:\.\d{1,3}){2})(?:[1-9]\d?|1\d\d|2[01]\d|22[0-3])(?:\.(?:1?\d{1,2}|2[0-4]\d|25[0-5])){2}(?:\.(?:[1-9]\d?|1\d\d|2[0-4]\d|25[0-4]))|(?:(?:[a-z\u00a1-\uffff0-9]-*)*[a-z\u00a1-\uffff0-9]+)(?:\.(?:[a-z\u00a1-\uffff0-9]-*)*[a-z\u00a1-\uffff0-9]+)*(?:\.(?:[a-z\u00a1-\uffff]{2,}))\.?)(?::\d{2,5})?(?:[/?#]\S*)?$/i;
+const UWURegex = /(https?)?(www)?uwu.whats-th\.is/i;
 
 /**
  * Handle Polr link shortens.
@@ -26,7 +27,7 @@ module.exports = (req, res) => {
   }
 
   // Check the URL in the request
-  if (!URLRegex.test(urlParsed.query['url'])) {
+  if (!URLRegex.test(urlParsed.query['url']) || UWURegex.test(urlParsed.query['url'])) {
     return res.end(400, 'Bad Request', 'invalid URL');
   }
 
