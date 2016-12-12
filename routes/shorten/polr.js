25f4b319eba9b985a25a9ade6d72207f0384cf8f
Dean Sheather <dean@deansheather.com>
2016-12-12 17:08:36 +1000

    change link shortener domain to awau.moe, made it configurable + version bump

--- routes/shorten/polr.js
+++ routes/shorten/polr.js
@@ -5,6 +5,9 @@ const S3 = require('../../lib/s3.js');
 const url = require('url');
 const util = require('../../lib/util.js');
 
+// Load configuration
+const config = require('../../config.json');
+
 // URL regex (taken from https://gist.github.com/dperini/729294)
 // Credit to @dperini
 const URLRegex = /^(?:(?:https?|ftp):\/\/)(?:\S+(?::\S*)?@)?(?:(?!(?:10|127)(?:\.\d{1,3}){3})(?!(?:169\.254|192\.168)(?:\.\d{1,3}){2})(?!172\.(?:1[6-9]|2\d|3[0-1])(?:\.\d{1,3}){2})(?:[1-9]\d?|1\d\d|2[01]\d|22[0-3])(?:\.(?:1?\d{1,2}|2[0-4]\d|25[0-5])){2}(?:\.(?:[1-9]\d?|1\d\d|2[0-4]\d|25[0-4]))|(?:(?:[a-z\u00a1-\uffff0-9]-*)*[a-z\u00a1-\uffff0-9]+)(?:\.(?:[a-z\u00a1-\uffff0-9]-*)*[a-z\u00a1-\uffff0-9]+)*(?:\.(?:[a-z\u00a1-\uffff]{2,}))\.?)(?::\d{2,5})?(?:[/?#]\S*)?$/i;
@@ -50,6 +53,6 @@ module.exports = (req, res) => {
     }
 
     // Return the URL to the client
-    return res.end(200, 'OK', 'https://uwu.whats-th.is/' + key);
+    return res.end(200, 'OK', config.linkShortenerPrefix + key);
   });
 };
