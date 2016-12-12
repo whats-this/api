a23919b8e17294d6092fde0f459bd27d8708f6e2
Dean Sheather <dean@deansheather.com>
2016-12-12 17:17:54 +1000

    fix: disallow awau.moe links being shortened + bump version

--- routes/shorten/polr.js
+++ routes/shorten/polr.js
@@ -11,7 +11,7 @@ const config = require('../../config.json');
 // URL regex (taken from https://gist.github.com/dperini/729294)
 // Credit to @dperini
 const URLRegex = /^(?:(?:https?|ftp):\/\/)(?:\S+(?::\S*)?@)?(?:(?!(?:10|127)(?:\.\d{1,3}){3})(?!(?:169\.254|192\.168)(?:\.\d{1,3}){2})(?!172\.(?:1[6-9]|2\d|3[0-1])(?:\.\d{1,3}){2})(?:[1-9]\d?|1\d\d|2[01]\d|22[0-3])(?:\.(?:1?\d{1,2}|2[0-4]\d|25[0-5])){2}(?:\.(?:[1-9]\d?|1\d\d|2[0-4]\d|25[0-4]))|(?:(?:[a-z\u00a1-\uffff0-9]-*)*[a-z\u00a1-\uffff0-9]+)(?:\.(?:[a-z\u00a1-\uffff0-9]-*)*[a-z\u00a1-\uffff0-9]+)*(?:\.(?:[a-z\u00a1-\uffff]{2,}))\.?)(?::\d{2,5})?(?:[/?#]\S*)?$/i;
-const UWURegex = /(https?)?(www)?uwu.whats-th\.is/i;
+const UWURegex = /(?:https?:\/\/)(?:www)?(uwu.whats-th\.is|awau\.moe)/i;
 
 /**
  * Handle Polr link shortens.
